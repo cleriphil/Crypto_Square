339fc56135a7928631df21a118b71ceb695329bc
Cory Spitzer & cleri philiastides <student@epicodus.com>
2015-05-26 16:14:49 -0700

    Encrypts words that don't fit well in a square

--- js/scripts.js
+++ js/scripts.js
@@ -12,6 +12,19 @@ var preEncryptSplit = function(message) {
   var messagePieces = []
   for (var i = 0; i < size; i += 1) {
     messagePieces[i] = message.substring(i*size, (i+1) * size)
-  }
+  };
   return messagePieces
-}
+};
+
+var encrypt = function(messagePieces) {
+  // debugger;
+  var encryptedMessage = ""
+  for(var i = 0; i < messagePieces[0].length; i += 1) {
+    messagePieces.forEach(function(piece) {
+      if (piece[i]) {
+        encryptedMessage += piece[i]
+      }
+    });
+  };
+  return encryptedMessage
+};
--- spec/specs.js
+++ spec/specs.js
@@ -22,8 +22,21 @@ describe("preEncryptSplit", function() {
   it("returns an array of strings where elements have a max length of the square root of the ceiling of the given string", function() {
     expect(preEncryptSplit("boogoohoo")[2]).to.equal("hoo");
   });
+  it("returns an array of strings where elements have a max length of the square root of the ceiling of the given string", function() {
+    expect(preEncryptSplit("abcdefghijklmnopqrstuvw")[1]).to.equal("fghij");
+  });
 
   it("works without a perfect square", function() {
     expect(preEncryptSplit("Panda")[1]).to.equal("da")
   });
+});
+
+describe("encrypt", function() {
+  it("encrypts 'panda'", function() {
+    expect(encrypt(["pan", "dan"])).to.equal("pdaann")
+  })
+
+  it("encrypts 'portlandiscool'", function() {
+    expect(encrypt(["port", "land", "isco", "ol"])).to.equal("pliooaslrnctdo")
+  })
 });
