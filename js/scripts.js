6228e06ef8aac37b9ca09f28b292ab4021766cfe
Cory Spitzer & cleri philiastides <student@epicodus.com>
2015-05-26 15:26:31 -0700

    Test and define preEncryptSplit

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,17 @@
 var preEncrypt = function(message) {
-  return message.replace(/\s+/g, '')
+  message = message.replace(/\s+/g, '')   // combine regex
+  return message.replace(/[^\w\s]/g, '')
 };
+
+var squareSize = function(messageLength) {
+  return Math.ceil(Math.sqrt(messageLength))
+};
+
+var preEncryptSplit = function(message) {
+  var size = squareSize(message.length)
+  var messagePieces = []
+  for (var i = 0; i < size; i += 1) {
+    messagePieces[i] = message.substring(i*size, (i+1) * size)
+  }
+  return messagePieces
+}
--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,29 @@
 describe("preEncrypt", function() {
-  it("deletes the punctuation and spaces of a phrase", function() {
+  it("deletes white space in a phrase", function() {
     expect(preEncrypt("hi there")).to.equal("hithere");
+  });
+
+  it("removes punctuation", function() {
+    expect(preEncrypt("yo{u,y?o]).u!")).to.equal("youyou");
+  });
+});
+
+describe("squareSize", function() {
+  it("determines the size of the square", function() {
+    expect(squareSize(36)).to.equal(6);
+  });
+
+  it("determines the size given a imperfect square", function() {
+    expect(squareSize(37)).to.equal(7);
+  });
+});
+
+describe("preEncryptSplit", function() {
+  it("returns an array of strings where elements have a max length of the square root of the ceiling of the given string", function() {
+    expect(preEncryptSplit("boogoohoo")[2]).to.equal("hoo");
+  });
+
+  it("works without a perfect square", function() {
+    expect(preEncryptSplit("Panda")[1]).to.equal("da")
   });
 });
